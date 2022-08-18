b20c1cb97d015559d8919345675d4d657e60b263
abimont <abimontes00@gmail.com>
2022-08-18 14:27:37 -0700

    infinity loop fixed

--- shell.c
+++ shell.c
@@ -7,29 +7,35 @@
  */
 int main(int __attribute__((unused)) argc, char *argv[])
 {
-	char *line = NULL;
-	size_t buf_size = 0;
+	char *input = NULL;
+	size_t size = 0;
 	ssize_t characters = 0;
 
 	name = argv[0];
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO) == 1) /* if a fd is associated with terminal */
-			write(1, "< x_x > ", 8); /* write to standard output */
+		if (isatty(STDIN_FILENO) == 1) /* if a fd is a terminal */
+			write(1, "x_x $ ", 7); /* write to standard output */
 
-		characters = getline(&line, &buf_size, stdin);
+		if (characters == -1)
+		{
+			if (isatty(STDIN_FILENO) == 1)
+				write(1, "\n", 1);
+			break;
+		}
+		characters = getline(&input, &size, stdin);
 
-		if (line[characters - 1]  == '\n') /* when '\n', write '\0' */
-			line[characters - 1]  = '\0';
-		if (*line == '\0')
+		if (input[characters - 1]  == '\n') /* when '\n', write '\0' */
+			input[characters - 1]  = '\0';
+		if (*input == '\0')
 			continue;
-		if (command_read(line, characters) == 2) /* in case of exit */
+		if (command_read(input, characters) == 2) /* in case of exit */
 			break;
 	}
 
-	free(line);
-	line = NULL;
+	free(input);
+	input = NULL;
 
 	return (0);
 }
